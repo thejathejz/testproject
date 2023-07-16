1f4ab8deaf62d9dc5679db44712b03ff58a25c3a
theja krishna <theja@techversantinfo.com>
2023-07-16 19:19:27 +0530

    ecommerce

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 # accounts/urls.py
 
 from django.urls import path
-from .views import UserRegisterView,LoginView,CustomerListCreateView,CustomerRetrieveUpdateDeleteView,ProductListCreateView
+from .views import UserRegisterView,LoginView,CustomerListCreateView,CustomerRetrieveUpdateDeleteView,ProductListCreateView,ProductRetrieveUpdateDeleteView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('customers/', CustomerListCreateView.as_view(), name='customer-list'),
     path('customers/<int:pk>/', CustomerRetrieveUpdateDeleteView.as_view(), name='customer-list'),
     path('Product/', ProductListCreateView.as_view(), name='customer-list'),
-    path('Product/<int:pk>/', CustomerRetrieveUpdateDeleteView.as_view(), name='customer-list'),
+    path('Product/<int:pk>/',ProductRetrieveUpdateDeleteView.as_view(), name='customer-list'),
 ]
